5fa69ed21ad44f5c1a8c6e9b8a60b109ba335887
dolarshopify-dotcom <dolarshopify@gmail.com>
2026-07-22 02:15:26 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-alert("مرحبًا بك في متجري الإلكتروني!");
+const prices = {
+  "الجزائر": 600,
+  "وهران": 700,
+  "سطيف": 500,
+  "قسنطينة": 550
+};
+
+const wilaya = document.getElementById("wilaya");
+const deliveryPrice = document.getElementById("deliveryPrice");
+
+wilaya.addEventListener("change", function () {
+  const price = prices[this.value] || 0;
+  deliveryPrice.textContent = price;
+});
